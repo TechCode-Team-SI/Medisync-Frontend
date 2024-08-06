510d1957ec6b0600a2f93669968e7e164043252c
GDJTS23 <gdjtorrealba23@gmail.com>
2024-08-06 10:02:11 -0400

    prueba 3

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -78,7 +78,14 @@ const FormLabel = React.forwardRef<
 >(({ className, ...props }, ref) => {
   const { error, formItemId } = useFormField();
 
-  return <Label ref={ref} className={cn(error && 'text-red-500', className)} htmlFor={formItemId} {...props} />;
+  return (
+    <Label
+      ref={ref}
+      className={cn(error && 'text-red-500 dark:text-red-900', className)}
+      htmlFor={formItemId}
+      {...props}
+    />
+  );
 });
 FormLabel.displayName = 'FormLabel';
 
