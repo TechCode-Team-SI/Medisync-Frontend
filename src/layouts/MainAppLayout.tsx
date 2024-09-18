77f06e22ba84e9d2d4f7e87cadc9a9c587b565e8
chengkev <chengkev2000@gmail.com>
2024-09-18 06:27:30 -0400

    fix: bottom space overflow

--- src/layouts/MainAppLayout.tsx
+++ src/layouts/MainAppLayout.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from 'src/components/sidebar/sidebar';
 
 export const MainAppLayout = () => {
   return (
-    <div className='w-full h-full flex grow'>
+    <div className='w-full h-full flex grow pb-8'>
       <Sidebar />
       <Outlet />
     </div>
