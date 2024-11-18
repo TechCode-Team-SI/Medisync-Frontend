216fe69275233c62b7bae734c86b046e343890c4
chengkev <chengkev2000@gmail.com>
2024-11-18 15:36:17 -0400

    fix: add category for posts

--- src/components/modals/Post/schema.ts
+++ src/components/modals/Post/schema.ts
@@ -4,6 +4,12 @@ import { z } from 'zod';
 export const PostSchema = z.object({
   title: z.string().min(1, 'Ingrese el nombre'),
   description: z.string().min(1, 'Ingrese la descripcion'),
+  categories: z.array(
+    z.object({
+      id: z.string(),
+      name: z.string(),
+    }),
+  ),
 });
 
 export type PostSchema = z.infer<typeof PostSchema>;
