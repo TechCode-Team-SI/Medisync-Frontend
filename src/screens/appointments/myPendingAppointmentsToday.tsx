95069ea954c183dc46a949d7df30daa724d1dd97
GDJTS23 <gdjtorrealba23@gmail.com>
2024-11-20 01:15:50 -0400

    update request

--- src/screens/appointments/myPendingAppointmentsToday.tsx
+++ src/screens/appointments/myPendingAppointmentsToday.tsx
@@ -2,11 +2,13 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useDebounce } from '@uidotdev/usehooks';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 import PaginationController from 'src/components/common/pagination';
 import { Button } from 'src/components/ui/button';
 import { CardTitle } from 'src/components/ui/card';
 import Edit from 'src/components/ui/icons/edit';
+import Trash from 'src/components/ui/icons/trash';
 import { Loading } from 'src/components/ui/loading';
 import LoadingWrapper from 'src/components/wrappers/LoadingWrapper';
 import { MainContentWrapper } from 'src/components/wrappers/mainContentWrapper';
@@ -22,7 +24,11 @@ export function ListMyPendingAppointmentsToday() {
   const [searchTerm, setSearchTerm] = useState('');
   const debouncedSearchTerm = useDebounce(searchTerm, DEBOUNCE_DELAY);
   const [page, setPage] = useState(1);
-  const { data: appointment, isFetching } = useQuery({
+  const {
+    data: appointment,
+    isFetching,
+    refetch,
+  } = useQuery({
     queryKey: [debouncedSearchTerm, `${page}`, `get-all-my-pending-appointments`],
     queryFn: ({ queryKey }) =>
       RequestsHttp.getMyRequests({
@@ -39,6 +45,17 @@ export function ListMyPendingAppointmentsToday() {
       //TODO: move to the attend request screen
     },
     onError: (error) => {
+      console.log(error);
+    },
+  });
+  const CancelRequest = useMutation({
+    mutationFn: RequestsHttp.cancelRequest,
+    onSuccess: () => {
+      toast.success('Cita Cancelada Correctamente');
+      refetch;
+    },
+    onError: (error) => {
+      toast.success('La Cita no Fue Cancelada Correctamente');
       console.log(error);
     },
   });
@@ -79,20 +96,34 @@ export function ListMyPendingAppointmentsToday() {
               <CardTitle className='w-full font-roboto text-lg font-bold flex text-green-400 hover:text-green-400'>
                 {appointment.patientFullName}
               </CardTitle>
-              <Button
-                disabled={attendRequest.isPending}
-                onClick={() => {
-                  if (appointment.status === RequestStatusEnum.PENDING) {
-                    attendRequest.mutate({ id: appointment.id });
-                    navigate(paths.attendappointment, { state: appointment.id });
-                  } else {
-                    navigate(paths.attendappointment, { state: appointment.id });
-                  }
-                }}
-                className='absolute right-4 top-1/2 -translate-y-1/2 p-3 drop-shadow-md hover:drop-shadow-lg h-fit rounded-full bg-green-100 hover:bg-green-200'
-              >
-                <Edit className='fill-current text-green-400 h-4 w-4' />
-              </Button>
+              <div className='flex flex-row items-center space-x-4'>
+                <Button
+                  disabled={attendRequest.isPending}
+                  onClick={() => {
+                    if (appointment.status === RequestStatusEnum.PENDING) {
+                      attendRequest.mutate({ id: appointment.id });
+                      navigate(paths.attendappointment, { state: appointment.id });
+                    } else {
+                      navigate(paths.attendappointment, { state: appointment.id });
+                    }
+                  }}
+                  className='absolute right-16 top-1/2 -translate-y-1/2 p-3 drop-shadow-md hover:drop-shadow-lg h-fit rounded-full bg-green-100 hover:bg-green-200'
+                >
+                  <Edit className='fill-current text-green-400 h-4 w-4' />
+                </Button>
+
+                <Button
+                  disabled={CancelRequest.isPending}
+                  onClick={() => {
+                    if (appointment.status === RequestStatusEnum.PENDING) {
+                      CancelRequest.mutate({ requestId: appointment.id });
+                    }
+                  }}
+                  className='absolute right-4 top-1/2 -translate-y-1/2 p-3 drop-shadow-md hover:drop-shadow-lg h-fit rounded-full bg-green-100 hover:bg-green-200'
+                >
+                  <Trash className='fill-current text-green-400 h-4 w-4' />
+                </Button>
+              </div>
               <span className='text-gray-600 text-sm font-medium'>
                 {calculateAge(appointment.madeFor.birthday)} años | {getGenderLabel(appointment.madeFor.gender)}
               </span>
