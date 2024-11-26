66d20e13e318e0c729689458b658ebc6e1d6c654
GDJTS23 <gdjtorrealba23@gmail.com>
2024-11-26 02:49:42 -0400

    fix code dashboard

--- src/components/modals/Top/topElementDiagnosis.tsx
+++ src/components/modals/Top/topElementDiagnosis.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 
 import { Card, CardContent, CardTitle } from 'src/components/ui/card';
 import { DialogContent, DialogHeader, DialogTitle } from 'src/components/ui/dialog';
+import Injuries from 'src/components/ui/icons/injuries';
 import Logo from 'src/components/ui/icons/logo';
-import MedicalStaff from 'src/components/ui/icons/medicalStaff';
 import {
   Select,
   SelectContent,
@@ -57,7 +57,7 @@ export function TopElementDiagnosis() {
       <DialogHeader className='gap-4 flex flex-row mx-6 my-1 space-x-4 sm:space-x-6'>
         <Logo className='fill-current text-white h-[50px] w-[50px] sm:h-[69px] sm:w-[60px]' />
         <DialogTitle className='text-white font-montserrat text-[20px] sm:text-[24px] font-medium'>
-          Ranking Estadístico <span className='font-bold'>por Categorías de Diagnóstico Médico</span>
+          Ranking Estadístico por<span className='font-bold'>Categorías de Diagnóstico Médico</span>
         </DialogTitle>
       </DialogHeader>
       <CardContent className='w-full h-full flex flex-col overflow-auto scrollbar-edit rounded-b-xl bg-white p-3 sm:p-6 lg:p-6 gap-5'>
@@ -106,7 +106,7 @@ export function TopElementDiagnosis() {
               className='w-full h-24 bg-white border-[0.2px] border-black/5 shadow-lg shadow-black/30 flex flex-row items-center p-5 gap-5'
             >
               <div className='flex-shrink-0 h-[55px] w-[55px] rounded-full bg-green-400 flex flex-col overflow-hidden items-center justify-center'>
-                <MedicalStaff className='fill-current text-white h-[30px] w-[35px]' />
+                <Injuries className='fill-current text-white h-[30px] w-[35px]' />
               </div>
               <div className='flex flex-col gap-1 grow'>
                 <CardTitle className='font-roboto font-bold text-[18px] text-green-400'>{Doc.name}</CardTitle>
